72d745f2460c83ab8d7ae3ca92be056776684d1f
jurekg1 <jurek@poczta.pl>
2024-12-03 14:54:31 +0100

    first

---
+++ BoardGame/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Console;
+
+internal class Program
+{
+
+    public class Player
+    {
+        public string 
+    }
+
+
+
+public static void Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+    }
+}
